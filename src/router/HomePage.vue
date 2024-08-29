4d933fc10ecd5ebd069bd54e33203c377a45bda4
joaovnpt <joaovitorguiotti8@gmail.com>
2024-08-29 13:13:40 -0300

    Feat #6: Criar layout de Cards na HomePage

--- src/router/HomePage.vue
+++ src/router/HomePage.vue
@@ -1,13 +1,28 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <NavbarComponent/>
-  <BannerComponent/>
-  <CardSectionTitle/>
-  <CardComponent/>
+  <NavbarComponent />
+  <BannerComponent />
+  <CardSectionTitle />
+  <div class="card-container">
+    <CardComponent />
+    <CardComponent />
+    <CardComponent />
+    <CardComponent />
+    <CardComponent />
+    <CardComponent />
+    <CardComponent />
+    <CardComponent />
+    <CardComponent />
+  </div>
 </template>
 
 <style scoped>
-
+.card-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  row-gap: 3rem;
+}
 </style>
