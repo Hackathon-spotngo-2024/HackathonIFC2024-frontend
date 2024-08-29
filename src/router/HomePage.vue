96f747764d14d6df3734f0ffb3f1a7df49282bcc
joaovnpt <joaovitorguiotti8@gmail.com>
2024-08-29 12:38:00 -0300

    Feat #6: Adicionar CardComponent na HomePage

--- src/router/HomePage.vue
+++ src/router/HomePage.vue
@@ -1,11 +1,11 @@
 <script setup>
-import NavbarComponent from '@/components/NavbarComponent.vue';
-import BannerComponent from '@/components/BannerComponent.vue';
 </script>
 
 <template>
   <NavbarComponent/>
   <BannerComponent/>
+  <CardSectionTitle/>
+  <CardComponent/>
 </template>
 
 <style scoped>
