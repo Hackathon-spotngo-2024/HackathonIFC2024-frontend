d71779938377314ae3221f9101f73e92a1425962
joaovnpt <joaovitorguiotti8@gmail.com>
2024-08-28 22:11:18 -0300

    feat #6: Criar e estilizar componentes CardComponent e CardSectionTitle

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 import BannerComponent from './components/BannerComponent.vue';
 import NavbarComponent from './components/NavbarComponent.vue';
+import CardSectionTitle from './components/CardSectionTitle.vue';
+import CardComponent from './components/CardComponent.vue';
 </script>
 
 <template>
   <NavbarComponent/>
   <BannerComponent/>
+  <CardSectionTitle/>
+  <CardComponent/>
 </template>
 
 <style scoped>
---
+++ src/components/CardSectionTitle.vue
@@ -0,0 +1,17 @@
+<script setup>
+const cardSectionTitle = "Os favoritos para você"
+</script>
+
+<template>
+    <div class="card-section-title">{{ cardSectionTitle }}</div>
+</template>
+
+<style scoped>
+.card-section-title {
+    display: flex;
+    font-family: 'Montserrat';
+    font-size: 2rem;
+    font-weight: bold;
+    margin: 0 0 6rem 6rem;
+}
+</style>
