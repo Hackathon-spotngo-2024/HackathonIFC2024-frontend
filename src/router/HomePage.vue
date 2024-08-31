a9a7b33030534dcb521454b614db4cf688b066a5
joaovnpt <joaovitorguiotti8@gmail.com>
2024-08-31 08:56:48 -0300

    Excluir NavbarComponent da HomePage

--- src/router/HomePage.vue
+++ src/router/HomePage.vue
@@ -1,10 +1,8 @@
 <script setup>
-import NavbarComponent from '@/components/NavbarComponent.vue';
 import BannerComponent from '@/components/BannerComponent.vue';
 </script>
 
 <template>
-  <NavbarComponent/>
   <BannerComponent/>
 </template>
 
