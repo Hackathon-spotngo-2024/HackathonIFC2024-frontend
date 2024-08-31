b5e2c5aa02fe07564f6055a341eb7db888ef23cc
Math3usH <matheuhenriquecalsavara@gmail.com>
2024-08-31 11:18:29 -0300

    feat#8: criar form login

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import BannerComponent from './components/BannerComponent.vue';
 import NavbarComponent from './components/NavbarComponent.vue';
+import LoginComponent from './components/LoginComponent.vue';
 </script>
 
 <template>
   <NavbarComponent/>
   <BannerComponent/>
+  <LoginComponent/>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import App from './App.vue'
 import router from './router'
 import NavbarComponent from './components/NavbarComponent.vue';
 import BannerComponent from './components/BannerComponent.vue';
+import LoginComponent from './components/LoginComponent.vue';
 
 const app = createApp(App)
 app.component('NavbarComponent', NavbarComponent)
 app.component('BannerComponent', BannerComponent)
+app.component('LoginComponent', LoginComponent)
 
 app.use(router)
 
