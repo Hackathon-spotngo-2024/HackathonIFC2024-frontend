5ed088c4b15c7af118b7a3a3574a45048ec7ec46
joaovnpt <joaovitorguiotti8@gmail.com>
2024-09-25 00:34:49 -0300

    Feat #10: Substituir sistema de avanco de componentes manual para sistema de avanco com apenas um componente e mudando a key

--- src/views/AnunciarPage.vue
+++ src/views/AnunciarPage.vue
@@ -1,34 +1,56 @@
 <script setup>
-
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import AnunciarPasso1Component from '../components/AnunciarComponents/AnunciarPasso1Component.vue'
 import AnunciarPasso2Component from '../components/AnunciarComponents/AnunciarPasso2Component.vue'
 import AnunciarPasso3Component from '@/components/AnunciarComponents/AnunciarPasso3Component.vue'
 import AnunciarPasso4Component from '@/components/AnunciarComponents/AnunciarPasso4Component.vue'
 
-const etapaAtual = ref(1) //Obtém o valor da etapa atual, se não conseguir, é 1
+const etapaAtual = ref(1);
+
+const componenteAtual = computed(() => {
+  switch (etapaAtual.value) {
+    case 1:
+      return AnunciarPasso1Component;
+    case 2:
+      return AnunciarPasso2Component;
+    case 3:
+      return AnunciarPasso3Component;
+    case 4:
+      return AnunciarPasso4Component;
+    default:
+      return AnunciarPasso1Component;
+  }
+})
 
 function proximaEtapa() {
   etapaAtual.value++
-  localStorage.setItem('etapaAtual', etapaAtual.value) //"Seta" a etapa atual no localStorage do navegador
 }
 
 function etapaAnterior() {
-  
   etapaAtual.value--
-  localStorage.setItem('etapaAtual', etapaAtual.value)
 }
+
+
 </script>
 
 <template>
-<AnunciarPasso1Component :etapaAnterior="etapaAnterior" :proximaEtapa="proximaEtapa" v-if="etapaAtual === 1"/>
-<AnunciarPasso2Component :etapaAnterior="etapaAnterior" :proximaEtapa="proximaEtapa" v-if="etapaAtual === 2"/>
-<AnunciarPasso3Component :etapaAnterior="etapaAnterior" :proximaEtapa="proximaEtapa" v-if="etapaAtual === 3"/>
-<AnunciarPasso4Component :etapaAnterior="etapaAnterior" :proximaEtapa="proximaEtapa" v-if="etapaAtual === 4"/>
+  <transition name="fade" mode="out-in">
+    <component
+      v-if="componenteAtual"
+      :is="componenteAtual"
+      :etapaAnterior="etapaAnterior"
+      :proximaEtapa="proximaEtapa"
+      :key="etapaAtual"
+    />
+  </transition>
 </template>
 
 <style scoped>
-.titulo-etapa-1 {
-    font-family: var(--fonte-principal);
+.fade-enter-active, .fade-leave-active {
+  transition: 300ms ease-in-out;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 </style>
