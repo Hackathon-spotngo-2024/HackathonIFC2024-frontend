adc5943067876da0290a2c66bd4b066580053763
gabrielcmohr <gabrielmohfeg@gmail.com>
2024-10-07 09:31:29 -0300

    feat #6: Criação do componente Reservacao

--- src/main.js
+++ src/main.js
@@ -7,7 +7,8 @@ import NavbarComponent from './components/NavbarComponent.vue';
 import BannerComponent from './components/BannerComponent.vue';
 import CardSectionTitle from './components/CardSectionTitle.vue';
 import CardComponent from './components/CardComponent.vue';
-import RodapeComponent from './components/RodapeComponent.vue'
+import RodapeComponent from './components/RodapeComponent.vue';
+import Reservacao from './components/Reservacao.vue'
 
 const app = createApp(App)
 app.component('NavbarComponent', NavbarComponent)
@@ -15,6 +16,7 @@ app.component('BannerComponent', BannerComponent)
 app.component('CardSectionTitle', CardSectionTitle)
 app.component('CardComponent', CardComponent)
 app.component('RodapeComponent', RodapeComponent)
+app.component('Reservacao', Reservacao)
 
 app.use(router)
 app.mount('#app')
--- src/views/AnuncioPage.vue
+++ src/views/AnuncioPage.vue
@@ -1,10 +1,14 @@
 <script setup>
+import Reservacao from '@/components/Reservacao.vue';
+
+
 
 </script>
 
 <template>
-
+    <Reservacao />
 </template>
+
 
 <style scoped>
 
