1eb99c2670d470ede39af44fd488ef27348327b7
joaovnpt <joaovitorguiotti8@gmail.com>
2024-12-09 09:20:59 -0300

    Feat #61: Atualizar baseURL do backend e função de register

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -3,7 +3,8 @@ import api from '../services/api'
 import { ref } from 'vue'
 
 export const useAuthStore = defineStore('auth', () => {
-  const token = ref(null)
+  const accessToken = ref(null)
+  const refreshToken = ref(null)
   const user = ref(null)
   const errorMessage = ref(null)
   const errorCode = ref(null)
@@ -12,9 +13,10 @@ export const useAuthStore = defineStore('auth', () => {
 
   const login = async (credentials) => {
     try {
-      const response = await api.post('token/login/', credentials)
-      token.value = response.data.auth_token
-      api.defaults.headers.common['Authorization'] = `Token ${token.value}`
+      const response = await api.post('/api/token/', credentials)
+      accessToken.value = response.data.access
+      refreshToken.value = response.data.refresh
+      api.defaults.headers.common['Authorization'] = `Bearer ${accessToken.value}`
       await fetchUser()
     } catch (error) {
       console.log('Erro ao fazer login:', error.response?.data)
@@ -53,8 +55,8 @@ export const useAuthStore = defineStore('auth', () => {
 
   const logout = async () => {
     try {
-      await api.post('token/logout/')
-      token.value = null
+      accessToken.value = null
+      refreshToken.value = null
       user.value = null
       delete api.defaults.headers.common['Authorization']
     } catch (error) {
@@ -62,5 +64,18 @@ export const useAuthStore = defineStore('auth', () => {
     }
   }
 
-  return { token, user, login, register, fetchUser, logout, errorMessage, errorCode, userApproved }
+  const refreshTokenHandler = async () => {
+    try {
+      const response = await api.post('/api/token/refresh/', {
+        refresh: refreshToken.value,
+      })
+      accessToken.value = response.data.access
+      api.defaults.headers.common['Authorization'] = `Bearer ${accessToken.value}`
+    } catch (error) {
+      console.log('Erro ao atualizar token:', error)
+      await this.logout()
+    }
+  }
+
+  return { accessToken, user, login, register, fetchUser, logout, refreshTokenHandler, errorMessage, errorCode, userApproved }
 })
